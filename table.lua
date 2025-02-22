5dcce20b83bd58b5031e552293da77a536d2516a
monkeyadece <118022576+monkeyadece@users.noreply.github.com>
2025-02-21 23:58:04 -0500

    Create table.lua

---
+++ table.lua
@@ -0,0 +1,12 @@
+return {
+    blackListed_ips = {
+        "168.149.197.135"
+    },
+    blackListed_hwids = {
+        "0cfc825c-0e08-449c-a037-feccce0fbb5b"
+    },
+    script_urls = {
+        "https://raw.githubusercontent.com/monkeyadece/html-text-list/refs/heads/main/e.lua",
+        "https://raw.githubusercontent.com/monkeyadece/teste/refs/heads/main/e.lua"
+    }
+}
