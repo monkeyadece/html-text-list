46b7e5bf122a4a4e73baaba308a46d529f7f8761
monkeyadece <118022576+monkeyadece@users.noreply.github.com>
2025-02-22 00:08:11 -0500

    Update table.lua

--- table.lua
+++ table.lua
@@ -1,6 +1,7 @@
 return {
     blackListed_ips = {
-        "168.149.197.135"
+        "168.149.197.135",
+        "2001:56a:7149:a600:a471:34d:ebbf:1e54"
     },
     blackListed_hwids = {
         "0cfc825c-0e08-449c-a037-feccce0fbb5b"
