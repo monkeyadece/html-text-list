c618211af6475ab4400d9ee53f1bbcf1ea6858a7
monkeyadece <118022576+monkeyadece@users.noreply.github.com>
2025-02-22 23:59:36 +0000

    Update table.lua

--- table.lua
+++ table.lua
@@ -1,9 +1,9 @@
 return {
     blackListed_ips = {
-        "168.149.197.135"
+        ""
     },
     blackListed_hwids = {
-        "0cfc825c-0e08-449c-a037-feccce0fbb5b",
+        "",
         "6e63bd95-d482-44b4-90ba-7ed614e68753"
     },
     script_urls = {
